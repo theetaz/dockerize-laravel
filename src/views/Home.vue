6f7685bcb2ed0d09a42a7fc0d4be01daf7fe4c56
amila <amilansinghe@gmail.com>
2021-06-12 23:59:49 +0530

    add twitter button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
         </div>
       </b-col>
     </b-row>
+
     <swap-carousel />
     <b-tabs content-class="mt-1">
       <b-tab
@@ -116,17 +117,35 @@
         </b-row>
       </b-tab>
     </b-tabs>
+    <ul class="social-networks spin-icon">
+      <li>
+        <a
+          href="https://twitter.com/rugfreecoins"
+          class="icon-twitter"
+          target="_blank"
+          title="Join us on Twitter"
+          >Twitter</a
+        >
+      </li>
+    </ul>
+    <!-- <div class="translucent-wrap bg-primary rounded-bottom p-1"> -->
+    <div class="d-flex justify-content-left text-center">
+      <b-toast v-model="visible" static no-auto-hide>
+        <template #toast-title>
+          <div class="d-flex align-items-center  test-toast">
+            <strong class="mr-auto"
+              >We are looking for grow our marketing team. Contact live chat if
+              iterested 🙏</strong
+            >
+          </div>
+        </template>
+      </b-toast>
+    </div>
   </div>
 </template>
 
 <script>
-import {
-  BRow,
-  BCol,
-  BSpinner,
-  BTabs,
-  BTab,
-} from "bootstrap-vue";
+import { BRow, BCol, BSpinner, BTabs, BTab, BToast } from "bootstrap-vue";
 // import Carousel from "@core/components/carousel/Carousel.vue";
 import CryptoTable from "../views/crypto/CryptoTable";
 import { mixinList } from "@/mixins/mixinList";
@@ -141,11 +160,13 @@ export default {
     CryptoTable,
     BTabs,
     BTab,
-    SwapCarousel
+    SwapCarousel,
+    BToast,
   },
   data() {
     return {
       loading: false,
+      visible: true,
     };
   },
   computed: {
@@ -195,6 +216,12 @@ export default {
 };
 </script>
 <style>
+.b-toast .toast-header {
+    background-color: #0057b1 !important;
+    width: 831px !important;
+    font-size: large !important;
+    border-radius: 10px !important;
+}
 @media (max-width: 1024px) {
   img {
     border-style: none;
@@ -220,6 +247,187 @@ img {
   .nav-tabs .nav-link {
     font-weight: 600;
     font-size: large;
+  }
+}
+</style>
+<style lang="scss" scoped>
+
+
+// .test-toast {
+//   background-color: #007af9 !important;
+//     width: 831px !important;
+//     font-size: large !important;
+//     border-radius: 10px !important;
+// }
+.translucent-wrap {
+  height: 110px;
+  position: relative;
+}
+@font-face {
+  font-family: "icomoon";
+  src: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/93/icomoon.eot?3qkin2");
+  src: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/93/icomoon.eot?#iefix3qkin2")
+      format("embedded-opentype"),
+    url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/93/icomoon.woff?3qkin2")
+      format("woff"),
+    url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/93/icomoon.ttf?3qkin2")
+      format("truetype"),
+    url("fhttps://s3-us-west-2.amazonaws.com/s.cdpn.io/93/icomoon.svg?3qkin2#icomoon")
+      format("svg");
+  font-weight: normal;
+  font-style: normal;
+}
+
+.social-networks {
+  margin: 0 0 35px;
+  padding: 0;
+  font-size: 0;
+  text-align: center;
+
+  li {
+    display: inline-block;
+  }
+
+  a {
+    display: block;
+    position: relative;
+    margin: 0 7px;
+    width: 68px;
+    height: 68px;
+    font-size: 0;
+    line-height: 0;
+    text-align: center;
+    text-decoration: none;
+
+    &:before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 100%;
+      height: 100%;
+      margin-top: -50%;
+      margin-left: -50%;
+      border-radius: 100px;
+      background-color: #1f2027;
+      transition: all 400ms;
+    }
+
+    &:after {
+      position: relative;
+      display: block;
+      font-family: "icomoon";
+      speak: none;
+      font-style: normal;
+      font-weight: normal;
+      font-variant: normal;
+      text-transform: none;
+      line-height: 1;
+      color: #fff;
+      font-size: 26px;
+      line-height: 66px;
+    }
+
+    &:hover,
+    &:focus {
+      text-decoration: none;
+    }
+  }
+
+  &.spin-icon a:hover:after,
+  &.spin-icon a:focus:after {
+    transition: transform 0.4s ease-out;
+    transform: rotateZ(360deg);
+  }
+
+  &.square a:hover:before,
+  &.square a:focus:before {
+    border-radius: 0;
+  }
+
+  &.bounce a:hover,
+  &.bounce a:focus {
+    animation: bounce 1s;
+  }
+
+  $networks: linkedin "\eac9"#0077b5, facebook "\e603"#3b5998,
+    twitter "\e618"#00b0ed, github "\eab1"#4183c4, instagram "\e604"#3f729b,
+    vimeo "\ea9c"#1ab7ea, twitch "\ea9b"#6441a5, pinterest "\e605"#cc2127;
+
+  @each $network in $networks {
+    .icon-#{nth($network, 1)} {
+      &:after {
+        content: "#{nth($network, 2)}";
+      }
+
+      &:hover:before,
+      &:focus:before {
+        background: nth($network, 3);
+      }
+    }
+  }
+}
+
+@keyframes bounce {
+  0%,
+  20%,
+  53%,
+  80%,
+  100% {
+    transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    transform: translate3d(0, 0, 0);
+  }
+
+  40% {
+    transition-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+    transform: translate3d(0, -6px, 0);
+  }
+
+  70% {
+    transition-timing-function: cubic-bezier(0.755, 0.05, 0.855, 0.06);
+    transform: translate3d(0, -4px, 0);
+  }
+
+  90% {
+    transform: translate3d(0, -2px, 0);
+  }
+}
+
+/* Extra stuff just to make the demo pretty. */
+body {
+  padding: 35px 0;
+  color: #fff;
+  font-family: "Lora", serif;
+  font-size: 14px;
+  text-align: center;
+  line-height: 24px;
+  background: #262730
+    url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/93/noise-bg.png) repeat 0 0;
+}
+
+h1 {
+  margin-bottom: 0px;
+  font-size: 55px;
+  line-height: 1;
+}
+
+h2 {
+  font-family: "Montserrat", sans-serif;
+  margin-bottom: 45px;
+  font-size: 12px;
+  text-transform: uppercase;
+}
+
+footer {
+  margin: 75px 0 35px;
+}
+
+a {
+  color: #4183c4;
+  text-decoration: none;
+
+  &:hover {
+    color: lighten(#4183c4, 15%);
   }
 }
 </style>
